0a2df67ddd66048731e836c07ca0f4549561420d
Miha Zelnik <miha.zelnik@gmail.com>
2016-06-09 20:42:51 +0200

    refresh token command

--- src/adwordspy/cli.py
+++ src/adwordspy/cli.py
@@ -14,19 +14,42 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+from __future__ import print_function
+
 import sys
 
+from oauth2client import client
 
 def main(argv=sys.argv):
     """
-    Args:
-        argv (list): List of arguments
-
-    Returns:
-        int: A return code
-
-    Does stuff.
+        Retrieve and display the access and refresh token.
     """
+    if len(argv) < 3:
+        print('CLIENT_ID or CLIENT_SECRET is missing')
+        return 0
 
-    print(argv)
-    return 0
+    client_id = argv[1]
+    client_secret = argv[2]
+    flow = client.OAuth2WebServerFlow(
+      client_id=client_id,
+      client_secret=client_secret,
+      scope=['https://www.googleapis.com/auth/adwords'],
+      user_agent='Ads Python Client Library',
+      redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+
+    authorize_url = flow.step1_get_authorize_url()
+
+    print('Log into the Google Account you use to access your AdWords account'
+        'and go to the following URL: \n{}\n'.format(authorize_url))
+    print('After approving the token enter the verification code (if specified).')
+    code = input('Code: ').strip()
+
+    try:
+        credential = flow.step2_exchange(code)
+    except client.FlowExchangeError as e:
+        print('Authentication has failed: {}'.format(e))
+        sys.exit(1)
+    else:
+        print('OAuth 2.0 authorization successful!\n\n'
+              'Your access token is:\n {}\n\nYour refresh token is:\n {}'.format(
+              credential.access_token, credential.refresh_token))
